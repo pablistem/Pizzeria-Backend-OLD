2aec109728d285ca3e99eb8759515e09deb8445d
Leandro Medina <medinaleandron@gmail.com>
2023-01-27 21:39:56 -0300

    Email service mocked for development

--- src/modules/message/application/service/message.service.ts
+++ src/modules/message/application/service/message.service.ts
@@ -1,5 +1,34 @@
 import { MessageRepository } from "../../infrastructure/message.repository";
-
+import sgMail  from '@sendgrid/mail'
+import { MailService } from "@sendgrid/mail/src/mail";
 export class MessageService {
-  constructor(MessageRepository: MessageRepository) {}
+  mailingProvider: MailService
+  constructor(MessageRepository: MessageRepository) {
+    sgMail.setApiKey(String(process.env.SENDGRID_API_KEY))
+    this.mailingProvider = sgMail
+  }
+  async sendMail(subject:string, to:string, template:string){
+    const msg = {
+      to: to, // Change to your recipient
+      from: 'pizzadonremolo@gmail.com', // Change to your verified sender
+      subject: subject,
+      text: template,
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    }
+    /*
+    this.mailingProvider
+      .send(msg)
+      .then(() => {
+      console.log('Email sent')
+    })
+      .catch((error) => {
+      console.error(error)
+    })
+  }
+   */
+ return true
 }
+
+}
+
+
